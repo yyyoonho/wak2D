2552ec4643be8554f430b0a94ce2394727831b67
DESKTOP <rotmzmfh@gmail.com>
2021-01-12 13:15:22 +0900

    feat: 순찰하는 적과 TestScene, 테스트용 Sprite 추가

---
+++ Assets/Scripts/Enemy_Attack_Component.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Attack_Component : MonoBehaviour
+{
+    public bool isRanged;
+    public float TimeBetAttack;
+    public GameObject projectile;
+
+    private GameObject _player;
+    private Enemy_Sprite_Component enemy_Sprite_Component;
+    private float attackTimer;
+    private bool isAttacking = false;
+
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        enemy_Sprite_Component = GetComponent<Enemy_Sprite_Component>();
+        attackTimer = TimeBetAttack;
+    }
+
+    public void RefreshAttackCycle()
+    {
+        attackTimer = TimeBetAttack;
+        isAttacking = false;
+    }
+
+    public void Attack_Update()
+    {
+        attackTimer += Time.deltaTime;
+        if (attackTimer >= TimeBetAttack && !isAttacking)
+        {
+            attackTimer = 0f;
+            if (isRanged)
+            {
+                //원거리 공격인 경우
+            }
+            else
+            {
+                enemy_Sprite_Component.LookAtTarget(_player.transform);
+                StartCoroutine(AttackCoroutine());
+            }
+        }
+    }
+
+    private IEnumerator AttackCoroutine()
+    {
+        isAttacking = true;
+
+        //공격 코드
+
+        yield return new WaitForSeconds(0.5f);
+
+        Debug.Log("Attacked");
+
+        isAttacking = false;
+
+        attackTimer = 0f;
+    }
+}
---
+++ Assets/Scripts/Enemy_Sprite_Component.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Sprite_Component : MonoBehaviour
+{
+    public Transform lookingPoint;
+
+    private bool lookingRight;
+
+    public void SwapSide()
+    {
+        lookingRight = !lookingRight;
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+    }
+
+    public void LookAtTarget(Transform _target)
+    {
+        if(transform.position.x < _target.position.x)
+        {
+            if(!lookingRight)
+            {
+                SwapSide();
+            }
+        }
+        else
+        {
+            if(lookingRight)
+            {
+                SwapSide();
+            }
+        }
+    }
+
+    void Start()
+    {
+        if (lookingPoint.transform.position.x > transform.position.x) lookingRight = true;
+        else lookingRight = false;
+    }
+}
