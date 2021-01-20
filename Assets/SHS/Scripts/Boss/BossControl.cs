0f8bf6ac4e4407340976cf0fcf4ddc4f82d6eb76
shs0624 <rotmzmfh@gmail.com>
2021-01-20 21:27:41 +0900

    feat: Boss 1페이즈까지 추가

---
+++ Assets/SHS/Scripts/Boss/BossControl.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossControl : LivingEntity
+{
+    public List<float> PatternHPList;
+    public List<int> timeBetPattern;
+    public float teleportGap;
+    public Transform groundCheck;
+
+    private int patternIdx = 0;
+    private bool isLookingRight = false;
+    private GameObject _player;
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(HP <= PatternHPList[patternIdx])
+        {
+            patternIdx++;
+            _animator.SetTrigger("TriggerPattern");
+        }
+    }
+
+    public void TelePortToPlayer()
+    {
+        //0 = 왼쪽, 1= 오른쪽
+        int rand = Random.Range(0, 2);
+        if(rand == 0)
+        {
+            transform.position = _player.transform.position + new Vector3(-teleportGap, 0f, 0f);
+            if (!isLookingRight) SwapSide();
+        }
+        else
+        {
+            transform.position = _player.transform.position + new Vector3(teleportGap, 0f, 0f);
+            if (isLookingRight) SwapSide();
+        }
+    }
+
+    private void SwapSide()
+    {
+        isLookingRight = !isLookingRight;
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+    }
+}
---
+++ Assets/SHS/Scripts/Boss/Boss_Attack_Componenet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Attack_Componenet : MonoBehaviour
+{
+    public float attackDamage;
+    public Transform attackPoint;
+    public Vector2 hitBox;
+
+    private GameObject _player;
+    public bool isAttacking = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void MeleeAttack(float attackDamage)
+    {
+        Collider2D[] collider2Ds = Physics2D.OverlapBoxAll(attackPoint.position, hitBox, 0);
+        foreach (Collider2D coll in collider2Ds)
+        {
+            if (coll.CompareTag("Player"))
+            {
+                coll.GetComponent<LivingEntity>().GetDamaged(attackDamage);
+            }
+        }
+        isAttacking = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(attackPoint.position, hitBox);
+    }
+}
